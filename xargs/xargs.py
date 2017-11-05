721f01b76d007cdf08ba1da41bc304ad71452eb1
pete on ubuntu <859598732@qq.com>
2017-11-05 15:30:30 +0800

    Fix: xargs close bug

--- xargs/xargs.py
+++ xargs/xargs.py
@@ -8,7 +8,7 @@ import subprocess
 
 task_queue = queue.Queue()
 working_task = 0
-
+stdin_close = False
 
 def excute_task(bin_name, total_args, process_num):
     global working_task
@@ -19,7 +19,11 @@ def excute_task(bin_name, total_args, process_num):
 def excute_xargs(command, args_num, process_num):
     global stop
     total_args = []
-    for line in sys.stdin:
+    while True:
+        line = sys.stdin.readline()
+        if not line:
+            stdin_close = True
+            break
         total_args += line.split()
         if args_num == -1:
             excute_task(command, total_args, process_num)
@@ -62,9 +66,10 @@ if __name__ == '__main__':
 
     pool = pool.ThreadPool(worker_count + 1)
     p = pool.Process(None, target=excute_xargs, args=(args.x, args.args_num, args.process_num))
+    p.setDaemon(True)
     p.start()
     for i in range(worker_count):
         create_wroker(pool)
-    while working_task != 0:
+    while working_task != 0 or not stdin_close:
         pass
     exit(0)
